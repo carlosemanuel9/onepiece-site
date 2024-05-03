d9303a4949e7297725abf8cbcde7954cacf59307
Carlos Emanuel Rodrigues Queiroz <166170931+carlosemanuel9@users.noreply.github.com>
2024-05-03 20:49:42 -0300

    new

---
+++ script.js
@@ -0,0 +1,56 @@
+ScrollReveal().reveal('.head' , { 
+    origin:'top',
+    duration : 2000,
+    distance: '100%',
+    reset:'false'
+    });
+ScrollReveal().reveal('.section-1' , { 
+    origin:'left',
+    duration : 2000,
+    distance: '100%',
+    reset:'false'
+    });
+    ScrollReveal().reveal('.section-2' , { 
+        origin:'left',
+        duration : 2000,
+        distance: '100%',
+        reset:'false'
+        });
+        ScrollReveal().reveal('.section-3' , { 
+            origin:'left',
+            duration : 2000,
+            distance: '100%',
+            reset:'false'
+            });
+            ScrollReveal().reveal('.section-4' , { 
+                origin:'left',
+                duration : 2000,
+                distance: '100%',
+                reset:'false'
+                });
+                ScrollReveal().reveal('.section-5' , { 
+                    origin:'left',
+                    duration : 2000,
+                    distance: '100%',
+                    reset:'false'
+                    });
+                    
+                    ScrollReveal().reveal('.section-6' , { 
+                        origin:'left',
+                        duration : 2000,
+                        distance: '100%',
+                        reset:'false'
+                        });
+                        ScrollReveal().reveal('.img' , { 
+                            duration : 2000,
+                            distance: '100%',
+                            reset:'false',
+                            rotate:{x:0 ,y:80, x:0}
+                            });
+                            ScrollReveal().reveal('.img-ultima' , { 
+                                duration : 2000,
+                                distance: '100%',
+                                reset:'false',
+                                origin: 'left'
+                                });
+                        
